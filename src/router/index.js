4b99c9bbffa0c266c31468050ef8517a1767fb3e
hey-ri <kwonhr01@naver.com>
2023-02-18 22:27:35 +0900

    designate standard config

--- src/router/index.js
+++ src/router/index.js
@@ -13,7 +13,10 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    // 저장누를때 컨벤션 에러가 뜰 떄가 있는데,
+    // quote는 싱글 ' 로 써야 error가  나지 않는다. 처음 코딩 컨벤션, 스텐다드로 설정했는데 거기서 '를 쓰기 때문에 그렇게 써야한다.
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
   }
 ]
 
