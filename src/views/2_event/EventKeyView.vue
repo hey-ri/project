4dcf66a1f864e3fe5e4560b6426d533f2a317bb6
hey-ri <kwonhr01@naver.com>
2023-02-19 21:32:20 +0900

    key event binding

---
+++ src/views/2_event/EventKeyView.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <!-- <input type="search" v-model="searchText" @keyup="checkEnter($event)" /> -->
+    <input type="search" v-model="searchText" @keyup.enter="doSearch" />
+    <button @click="doSearch">조회</button>
+  </div>
+</template>
+<script>
+// @어쩌구에 .으로 바로 접근이 가능함
+// .enter
+// .tab
+// .delete
+// .esc
+// .space
+// .up
+// .down
+// .left
+// .right
+// .stop - event.stopPropagation()
+// .prevent - event.preventDefault()
+export default {
+  components: {},
+  data() {
+    return {
+      searchText: ''
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {
+    doSearch() {
+      console.log(this.searchText)
+    },
+    checkEnter(event) {
+      if (event.keyCode === 13) {
+        this.doSearch()
+      }
+    }
+  }
+}
+</script>
